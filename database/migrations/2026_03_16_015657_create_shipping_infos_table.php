608547b89271b2f34985de95105252e234e09aff
Ferry Iqbal Rhamdani <rhamdani128@gmail.com>
2026-03-27 13:04:13 +0700

    feat: Add `shipping_type` and `shipping_date` columns to the `shippings` table.

--- database/migrations/2026_03_16_015657_create_shipping_infos_table.php
+++ database/migrations/2026_03_16_015657_create_shipping_infos_table.php
@@ -11,18 +11,21 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('shippings', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('project_id')->constrained()->cascadeOnDelete();
-            $table->string('status')->default('Ongoing'); // Ongoing, Pending, Completed
-            $table->string('shipping_type')->default('Lengkap'); // Lengkap, Bertahap
-            $table->date('shipping_date')->nullable();
-            $table->timestamps();
+        Schema::table('shippings', function (Blueprint $table) {
+            if (!Schema::hasColumn('shippings', 'shipping_type')) {
+                $table->string('shipping_type')->default('Lengkap')->after('status');
+            }
+            if (!Schema::hasColumn('shippings', 'shipping_date')) {
+                $table->date('shipping_date')->nullable()->after('shipping_type');
+            }
         });
     }
 
     public function down(): void
     {
-        Schema::dropIfExists('shippings');
+        Schema::table('shippings', function (Blueprint $table) {
+            $table->dropColumn(['shipping_type', 'shipping_date']);
+        });
     }
+
 };
